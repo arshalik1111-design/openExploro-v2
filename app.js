b30507bd29657df6674bf734493f6d28ce03f6a9
Arsh Ali Khan <arshalik1111@gmail.com>
2025-12-25 23:08:16 +0530

    feat: implement CRUD functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js"); // we use this to test our lisitng sample
 
@@ -8,6 +8,7 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
   .then(() => {
@@ -20,11 +21,13 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+//Index route
+
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("./listings/index.ejs", { allListings });
 });
-//Create route, always above the dhow route otherwise the show route will take the listings/new as the the id passing.
+//Create route, always above the show route otherwise the show route will take the listings/new as the the id passing.
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
@@ -38,7 +41,40 @@ app.get("/listings/:id", async (req, res) => {
   res.render("listings/show.ejs", { listing });
 });
 
-//Index route
+//New list insertion Route
+app.post("/listings", async (req, res) => {
+  // let { title, description, image, price, location, country } = req.body;
+  // we have two ways either the upper one or the easy one, i.e. by making variables the key of the object Lisitng then we can do the below
+  let newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//Edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+// Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  // let listing = req.body;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+  res.redirect(`/listings/${id}`);
+})
+
+// Delete Route
+
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings")
+
+});
 
 //Show Route(Read in CRUD, for that we do: app.use(express.urlencoded(extended:true)) it is used to parse the incoming HTTP request bodies that are URL encoded. It makes the parsed data available as a javascript object on  the req.bpdy property )
 
@@ -62,5 +98,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen("3030", () => {
-  console.log("connection succesful");
+  console.log("Listening to port 3030");
 });
